a0b55d38e3d94df108310761f46b2e65b045b486
huqian301 <huqian301@gmail.com>
2020-09-30 19:13:21 -0700

    init

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3001, ()=>{
-    console.log('server started listening on PORT htto://localhhost:3001');
+app.listen(3001, () => {
+  console.log('server started listening on PORT htto://localhhost:3001');
 });

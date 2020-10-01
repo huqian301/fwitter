75a5112e22f4c9678696bcdfc01c17e7b0e597a9
huqian301 <huqian301@gmail.com>
2020-09-30 21:04:06 -0700

    Created post and get routes for users

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(routes);
 
 app.listen(3001, () => {
--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,9 +1,19 @@
 const router = require('express').Router();
+const connection = require('../../../config/connection');
 
 // /api/users prepended to every Route
 router.route('/')
-  .get((_req, res) => {
-    res.send('Hello');
+  .get(async (_req, res) => {
+    const query = 'SELECT * FROM users;';
+    const [rows] = await connection.query(query);
+    console.log('I am rows', rows);
+    res.json(rows);
+  })
+  .post(async (req, res) => {
+    const userInput = req.body;
+    const query = 'INSERT INTO users SET ?;';
+    const result = await connection.query(query, userInput);
+    res.json(result);
   });
 
 module.exports = router;

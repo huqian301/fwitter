82719e6f37a903c66b9632aeb7305c9a5e154f0f
huqian301 <huqian301@gmail.com>
2020-10-03 10:59:28 -0700

    Created insertToDb, insertUserQuery

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const { fetchUsers, insertUserToDb } = require('../model/userOrm');
+
+module.exports = {
+  getAllUsersApi: async (_req, res) => {
+    try {
+      const users = await fetchUsers();
+      res.json(users);
+    } catch (e) {
+      res.status(400).json(e);
+    }
+  },
+  insertUserApi: async (req, res) => {
+    const { username } = req.body;
+    try {
+      const result = await insertUserToDb(username);
+      res.json(result);
+    } catch (e) {
+      res.status(400).json(e);
+    }
+  },
+};
